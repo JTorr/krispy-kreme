10f8c3dfca3632617e7451c4df028e1d71f17c3e
Kevin Keller <kevbo30@gmail.com>
2020-11-28 11:03:31 -0600

    Add empty parser class

---
+++ src/com/juliekevin/Parser.java
@@ -0,0 +1,11 @@
+package com.juliekevin;
+
+import com.juliekevin.model.Command;
+
+public class Parser {
+
+    public Command parse(String command) {
+        return null;
+    }
+
+}
--- src/com/juliekevin/model/Command.java
+++ src/com/juliekevin/model/Command.java
@@ -1,5 +1,7 @@
 package com.juliekevin.model;
 
+import java.util.Objects;
+
 public class Command {
 
     private String verb = null;
@@ -10,8 +12,29 @@ public class Command {
         this.noun = noun;
     }
 
+    public String getVerb() {
+        return verb;
+    }
 
-    
+    public String getNoun() {
+        return noun;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Command command = (Command) o;
+        return Objects.equals(verb, command.verb) &&
+                Objects.equals(noun, command.noun);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(verb, noun);
+    }
+
     private Command(){
         //  Immutable
     }
