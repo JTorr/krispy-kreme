f98ad01a34f2f864ff99d6f26820acfe00663da7
Julie Torres <reeddetorres@gmail.com>
2021-01-25 21:35:49 -0500

    Add Mockito and Maven

--- src/utils/GameUtils.java
+++ src/utils/GameUtils.java
@@ -12,7 +12,12 @@ public class GameUtils {
 	
 	public static String getRandName() {
 		List<String> names = Game.readNameList();
-		int rand = getRand(1, names.size() - 1);
-		return names.get(rand);
+		try {
+			int rand = getRand(1, names.size() - 1);
+			return names.get(rand);
+		} catch(Exception e) {
+			System.out.println("ERROR: Name list was not loaded.");
+			return "Joe";
+		}
 	}
 }
--- test/com/juliekevin/CharacterTest.java
+++ test/com/juliekevin/CharacterTest.java
@@ -1,8 +1,14 @@
 package com.juliekevin;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import utils.GameUtils;
+
 
 class CharacterTest {
 
@@ -11,6 +17,9 @@ class CharacterTest {
 
 	@Test
 	void getInventory() {
-		assertTrue(player.getInventory().contains("Your inventory"));
+		try (MockedStatic<GameUtils> theMock = Mockito.mockStatic(GameUtils.class)) {
+		    theMock.when(GameUtils::getRandName).thenReturn("Rafael");
+		    assertTrue(player.getInventory().contains("Your inventory"));
+		  }
 	}
 }
